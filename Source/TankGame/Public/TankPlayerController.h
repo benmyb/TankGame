01129b00633d514c7e582376df56cb8fd3d08d6c
BenZalus <benzalus@gmail.com>
2018-05-08 11:25:25 +0800

    开始编写控制器逻辑

---
+++ Source/TankGame/Public/TankPlayerController.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "TankPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TANKGAME_API ATankPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
